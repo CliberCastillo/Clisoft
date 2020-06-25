4fc26a05382e5cef243e679cf9143b5250c1e6bb
cliber <clibercg@gmail.com>
2020-06-25 18:04:59 -0500

    agregando validaciones y tipos de retorno.

--- Clisoft.Api/Controllers/CargoController.cs
+++ Clisoft.Api/Controllers/CargoController.cs
@@ -1,6 +1,8 @@
 ﻿using Clisoft.Aplication;
 using Clisoft.Aplication.Interface.Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -16,35 +18,89 @@ namespace Clisoft.Api.Controllers
             _cargoAppService = cargoAppService;
         }
         [HttpGet]
-        public List<CargoDTO> GetAll()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<List<CargoDTO>> GetAll()
         {
-            return _cargoAppService.GetAll();
+            try
+            {
+                return _cargoAppService.GetAll();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
         [HttpGet("{id}")]
-        public async Task<CargoDTO> GetByIdAsync(string id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CargoDTO>> GetByIdAsync(string id)
         {
-            return await _cargoAppService.GetByIdAsync(id);
+            try
+            {
+                var cargoPorId = await _cargoAppService.GetByIdAsync(id);
+                if (cargoPorId == null)
+                {
+                    return NotFound();
+                }
+                return cargoPorId;
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
         [HttpPost]
-        public ActionResult Add([FromBody] CargoDTO cargo)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<CargoDTO> Add([FromBody] CargoDTO cargo)
         {
-            _cargoAppService.Add(cargo);
-            _cargoAppService.Save();
-            return Ok();
+            try
+            {
+                _cargoAppService.Add(cargo);
+                _cargoAppService.Save();
+                return CreatedAtAction(nameof(Add), new { id = cargo.IdCargo }, cargo);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
         [HttpPut("{id}")]
-        public ActionResult Update(string id, [FromBody] CargoDTO cargo)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<CargoDTO> Update(string id, [FromBody] CargoDTO cargo)
         {
-            _cargoAppService.Update(cargo);
-            _cargoAppService.Save();
-            return Ok();
+            try
+            {
+                if (cargo == null)
+                    return NotFound();
+                _cargoAppService.Update(cargo);
+                _cargoAppService.Save();
+                return cargo;
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult Delete(string id)
         {
-            _cargoAppService.Delete(id);
-            _cargoAppService.Save();
-            return Ok();
+            try
+            {
+                _cargoAppService.Delete(id);
+                _cargoAppService.Save();
+                return NoContent();
+
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
